66417d65db9197630312b5c8e8e70fc50bb88de1
thien0709 <thien070904@gmail.com>
2023-03-28 23:48:55 +0700

    update javascript

--- access/login.js
+++ access/login.js
@@ -1,37 +1,46 @@
 const change = document.querySelectorAll("#box h6");
 const change1 = document.querySelector(".change");
-for(let i =0;i<change.length;i++){
-    change[i].onclick = function(){
-        change1.classList.toggle("active");
-    }
+for (let i = 0; i < change.length; i++) {
+  change[i].onclick = function () {
+    change1.classList.toggle("active");
+  };
 }
 // Save value in storage
- //Get element login
- const nameLogin =document.querySelector("#login .name");
- const passLogin = document.querySelector("#login .pass");
- const loginBtn = document.querySelector("#login button");
- //Get element register
- const emailRegister = document.querySelector("#register .email");
- const userRegister = document.querySelector("#register .username");
- const passRegister = document.querySelector("#register .pass");
- const registerBtn = document.querySelector("#register button");
- //Save value in storage
- loginBtn.addEventListener("click", function(){
-    if(nameLogin.value === '' || passLogin.value === ''){
-        window.focus();
-        passLogin.focus();
-    }
-    localStorage.setItem('username', nameLogin.value.trim());
-    localStorage.setItem('password', passLogin.value.trim());
- });
- registerBtn.addEventListener("click",function(){
-    localStorage.setItem('userRegister', userRegister.value.trim());
-    localStorage.setItem('passRegister' , passRegister.value.trim());
-    localStorage.setItem('emailRegister',emailRegister.value.trim());
- })
- fetch('./access/main.json')
- .then(response => response.json())
- .then(data => 
-    console.log(data.address.street)
-   )
- .catch(error => console.error(error));
+//Get element login
+const nameLogin = document.querySelector("#login .name");
+const passLogin = document.querySelector("#login .pass");
+const loginBtn = document.querySelector("#login button");
+//Get element register
+const emailRegister = document.querySelector("#register .email");
+const userRegister = document.querySelector("#register .username");
+const passRegister = document.querySelector("#register .pass");
+const registerBtn = document.querySelector("#register button");
+//Save value in storage
+loginBtn.addEventListener("click", function () {
+  localStorage.setItem("username", nameLogin.value.trim());
+  localStorage.setItem("password", passLogin.value.trim());
+});
+registerBtn.addEventListener("click", function () {
+  localStorage.setItem("userRegister", userRegister.value.trim());
+  localStorage.setItem("passRegister", passRegister.value.trim());
+  localStorage.setItem("emailRegister", emailRegister.value.trim());
+});
+fetch("./access/main.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let count = 0;
+    loginBtn.addEventListener("click", function () {
+      let a = nameLogin.value.trim();
+      let b = passLogin.value.trim();
+      for (let i = 0; i < data.accounts.length; i++) {
+        if (a == data.accounts[i].username && b == data.accounts[i].password) {
+          count++;
+        }
+      }
+      if (count != 0) {
+        console.log("Da dang nhap thanh cong")
+      } else
+      console.log("Dang nhap khong thanh cong");
+    });
+  })
+  .catch((error) => console.error(error));
